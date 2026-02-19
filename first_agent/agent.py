991c192a24efb3eea5f6200b64a6bf5280203efd
Wojciech Kostański <wojtek.kostanski@gmail.com>
2026-02-19 16:43:11 +0100

    Create first_agent with basic configuration

---
+++ first_agent/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="first_agent",
+    model="gemini-2.0-flash",
+    description="My first agent",
+    instruction="""Jestes pomocnym asystentem ucznia odpowiadajacym jak sredniowieczny rycerz"""
+)
